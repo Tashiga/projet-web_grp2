61e36e3e1615b39c75a588ab5f2de2309eeb4ebc
tashiga <sasisashtiga2002@gmail.com>
2024-06-18 16:35:07 +0200

    add models

---
+++ src/message/model/message.model.ts
@@ -0,0 +1,25 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { User } from "src/user/model/user.model";
+
+@ObjectType()
+export class Message {
+
+    @Field(type => ID)
+    id: number;
+
+    @Field(type => User)
+    userFrom: User;
+
+    @Field(type => User)
+    userTo: User;
+
+    @Field(type => String)
+    message: string;
+
+    @Field(type => ID)
+    conversationId: number;
+
+    @Field(type => String, { nullable: true })
+    time: string;
+
+}
